c1b975cc41330ca86f3d6876e6b62ee2f86326d1
OMID <58745887+omidshayan@users.noreply.github.com>
2022-12-30 18:05:08 +0430

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+const searchBox = document.querySelector('.search-box')
+const btnSearch = document.querySelector('.btn-search')
+const inputSearch = document.querySelector('.input-search')
+
+
+btnSearch.addEventListener('click', () => {
+    searchBox.classList.toggle('active')
+    inputSearch.focus()
+})
