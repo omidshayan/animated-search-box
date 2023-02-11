e3e999e900ab53c173c860a375ea58d68d98332d
OMID <58745887+omidshayan@users.noreply.github.com>
2023-02-11 23:22:30 +0430

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
 let searchBox = document.querySelector('.search-box')
 let btnSearch = document.querySelector('.btn-search')
 let inputSearch = document.querySelector('.input-search')
-
-
 btnSearch.addEventListener('click', () => {
     searchBox.classList.toggle('active')
     inputSearch.focus()
